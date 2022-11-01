4bc298a052efb2a1a6bf87cd59f33b0667e586c9
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2022-10-31 23:14:29 -0300

    Setups cors on the server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,5 @@
 import Fastify from 'fastify'
+import cors from '@fastify/cors'
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient({
@@ -10,13 +11,17 @@ async function bootstrap() {
     logger: true,
   })
 
+  await fastify.register(cors, {
+    origin: true,
+  })
+
   fastify.get('/pools/count', async () => {
     const count = await prisma.pools.count()
 
     return { count }
   })
 
-  await fastify.listen({ port: 3333 })
+  await fastify.listen({ host: '0.0.0.0', port: 3333 })
 }
 
 bootstrap()
